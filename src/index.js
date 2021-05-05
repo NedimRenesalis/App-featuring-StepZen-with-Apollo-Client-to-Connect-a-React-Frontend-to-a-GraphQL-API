45cd86ea01255aa752c02683a1b2ee91c8e40d0e
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-05 13:06:54 +0200

    Modify index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { render } from "react-dom"
+import HomePage from "./pages/HomePage"
+
+render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
+
