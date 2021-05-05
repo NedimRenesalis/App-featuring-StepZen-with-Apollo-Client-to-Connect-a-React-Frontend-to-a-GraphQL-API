f76c3af469f3280397fc9844818e68d7671c3da7
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-05 14:07:29 +0200

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,9 +2,23 @@ import { useQuery } from "@apollo/react-hooks"
 import { GET_USERS_QUERY } from "../queries/getUsers.js"
 
 export default function Users() {
+  const {
+    data,
+    loading,
+    error
+  } = useQuery(GET_USERS_QUERY)
+
+  const users = data?.getUsers
+
+  if (loading) return <p>Almost there...</p>
+  if (error) return <p>{error.message}</p>
+
   return (
     <>
       <h2>Users</h2>
+      <pre>
+        {JSON.stringify(users, null, "  ")}
+      </pre>
     </>
   )
 }
